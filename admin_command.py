9e0d70e9f080bb6459c0554d4abe17de72e7fd7b
2010person <116949561+2010person@users.noreply.github.com>
2024-02-18 12:40:07 +0000

    Revert "Full functionality for dropping keys available "

--- admin_command.py
+++ admin_command.py
@@ -4,27 +4,23 @@ try:
     def admin():
         print("Greeting, admin, you have unlocked admin mode!")
         while 0==0:
-            ad_level = int(input("""Which level do you wish to skip to (the doors and torches will be unlocked for you)?
-WARNING!!! Not entering an integer will result in the game being terminated, due to an error """))
-        while 0==0:
-            if ad_level == 0 or ad_level == 1:
-                print("Goodbye, admin, off to Level", ad_level, "you go!")
-                for x in range (ad_level + 1):
-                    p1.house[x][2] = True
-                    p1.house[x][3] = False
-                if ad_level == 0:
-                    p1.level_0()
-                    break
-                elif ad_level == 1:
-                    p1.level_1()
-                    break
-                elif ad_level == 2:
-                    p1.level_2()
-                    break
+            ad_level = int(input("Which level do you wish to skip to (the doors and torches will be unlocked for you)?"))
+            while 0==0:
+                if ad_level == 0 or ad_level == 1:
+                    print("Goodbye, admin, off to Level", ad_level, "you go!")
+                    for x in range (ad_level + 1):
+                        p1.house[x][2] = True
+                        p1.house[x][3] = False
+                    if ad_level == 0:
+                        p1.level_0()
+                        break
+                    elif ad_level == 1:
+                        p1.level_1()
+                        break
+                    else:
+                        print("That isn't a valid level try again!")
                 else:
-                    print("That isn't a valid level try again!")
-            else:
-                print("Unfortunately, that level dosen't exist. Please try again!")
+                    print("Unfortunately, that level dosen't exist. Please try again!")
 
 except:
     print("""Apologies admin, an error has occured!
