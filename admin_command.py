2b3214f10a760a2ff14bc20aee206cf395294aab
2010person <116949561+2010person@users.noreply.github.com>
2024-02-25 22:02:53 +0000

    Pending changes exported from your codespace

--- admin_command.py
+++ admin_command.py
@@ -1,28 +1,35 @@
 import sys # Default module so you should have it
 import part_1_the_beginning as p1
-try:
-    def admin():
-        print("Greeting, admin, you have unlocked admin mode!")
-        while 0==0:
-            ad_level = int(input("Which level do you wish to skip to (the doors and torches will be unlocked for you)?"))
-            while 0==0:
-                if ad_level == 0 or ad_level == 1:
-                    print("Goodbye, admin, off to Level", ad_level, "you go!")
-                    for x in range (ad_level + 1):
-                        p1.house[x][2] = True
-                        p1.house[x][3] = False
-                    if ad_level == 0:
-                        p1.level_0()
-                        break
-                    elif ad_level == 1:
-                        p1.level_1()
-                        break
-                    else:
-                        print("That isn't a valid level try again!")
-                else:
-                    print("Unfortunately, that level dosen't exist. Please try again!")
+#try:
+def admin():
+    print("Greeting, admin, you have unlocked admin mode!")
+    while 0==0:
+        ad_level = int(input("""Which level do you wish to skip to (the doors and torches will be unlocked for you)?
+WARNING!!! Not entering an integer will result in the game being terminated, due to an error """))
+    while 0==0:
+        if ad_level == 0 or ad_level == 1:
+            print("Goodbye, admin, off to Level", ad_level, "you go!")
+            for x in range (ad_level + 1):
+                p1.house[x][2] = True
+                p1.house[x][3] = False
+            if ad_level == 0:
+                p1.level_0()
+                break
+            elif ad_level == 1:
+                p1.level_1()
+                break
+            elif ad_level == 2:
+                p1.level_2()
+                break
+            elif ad_level == 3:
+                p1.level_3()
+                break
+            else:
+                print("That isn't a valid level try again!")
+        else:
+            print("Unfortunately, that level dosen't exist. Please try again!")
 
-except:
-    print("""Apologies admin, an error has occured!
-Please look in into it!""")
-    sys.exit(1)
+#except:
+#    print("""Apologies admin, an error has occured!
+#Please look in into it!""")
+ #   sys.exit(1)
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys # Default module so you should have it 
 import part_1_the_beginning as p1 # Imported the part 1 python file as "p1"
-try: 
-    p1.intro() # Called the intro() function from the part 1 python file
-    print("As this game is still in development, you have reached the END.")
-                
-except:
-    print("I apolgize, but there has been an error in the game...")
-    print("Please report this error to Horror Games People Industries.")
-    print("Take a screenshot and tag us on X!")
-    print("I am afraid its GAME OVER! Please hold on while we resolve the problem...")
-    sys.exit(1)
+#try: 
+p1.intro() # Called the intro() function from the part 1 python file
+print("As this game is still in development, you have reached the END.")
+
+#except:
+ #   print("I apolgize, but there has been an error in the game...")
+  #  print("Please report this error to Horror Games People Industries.")
+   # print("Take a screenshot and tag us on X!")
+    #print("I am afraid its GAME OVER! Please hold on while we resolve the problem...")
+    #sys.exit(1)
